959855ed7a2e33576da51b2f4996b7c95c06bc0e
bora <bora@DESKTOP-5I0LV8B>
2018-06-03 16:57:22 +0300

    HesapMakinesi ManagedBean eklendi.

---
+++ src/main/java/com/bilisimegitim/hesapmakinesijsf/mb/HesapMakinesi.java
@@ -0,0 +1,20 @@
+package com.bilisimegitim.hesapmakinesijsf.mb;
+
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author bora
+ */
+@Named(value = "hesapMakinesi")
+@RequestScoped
+public class HesapMakinesi {
+
+    /**
+     * Creates a new instance of HesapMakinesi
+     */
+    public HesapMakinesi() {
+    }
+    
+}
